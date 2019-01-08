2362bebebb58c96bf8908ff18eb078acbcfdb623
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2019-01-09 00:31:57 +0530

    Added Sqlite database and showed content fron database onto home page

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-            {
-                'author' : 'Anubhav',
-                'title' : 'Post 1',
-                'content' : 'Working on new django project',
-                'date_posted' : 'January 7, 2018'
-            },
-            {
-                'author' : 'Saubhagya',
-                'title' : 'Post 2',
-                'content' : 'Playing PUBG',
-                'date_posted' : 'January 7, 2018'
-            }
-
-]
+from .models import Post
 
 def home(request):
 
     context = {
-                'posts'  : posts
+                'posts'  : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
